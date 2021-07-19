e648013dc8bfcfad85a6d5587e078e93e455934d
zhoubangfu <zbfcqtl@163.com>
2021-07-19 09:39:21 +0800

    build:v1.1.1

--- docs/MdEditor/utils/dom.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * 设置页面元素可移动
- *
- * @param trigger 触发器，通过该元素移动父级元素
- */
-export declare const keyMove: (trigger: HTMLElement, moveHandler?: ((left: number, top: number) => void) | undefined) => (() => void);
